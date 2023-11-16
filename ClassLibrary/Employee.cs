cd5fadeaaa0622c32c95eee01ddf0036a2547618
Dimonti <d1mont1@mail.ru>
2023-11-16 17:28:50 +0600

    Добавьте файлы проекта.

---
+++ ClassLibrary/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public struct Employee
+    {
+        public string Name;       // Поле для имени сотрудника
+        public Vacancies Vacancy; // Поле для должности
+        public int[] HireDate;    // Поле для даты приема на работу (год, месяц, день)
+        public int Salary;        // Поле для зарплаты
+
+        // Метод для вывода информации о сотруднике
+        public override string ToString()
+        {
+            return $"Name: {Name}, Vacancy: {Vacancy}, Hire Date: {string.Join("-", HireDate)}, Salary: {Salary}";
+        }
+    }
+}
---
+++ dotNET-module-11-practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace dotNET_module_11_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1;
+            emp1.Name = "Dinmukhammed Omirzak";
+            emp1.Vacancy = Vacancies.Manager;
+            emp1.HireDate = new int[] { 2023, 11, 16 };
+            emp1.Salary = 50000;
+
+            Console.WriteLine(emp1.ToString());
+        }
+    }
+}
